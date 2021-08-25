10a627a7cf3a820668214456e013c347d3ed6907
Manu Arora <manuarorawork@gmail.com>
2021-08-25 13:44:36 +0530

    ⚙️ Fix: Make to and from emails as the same one

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -6,7 +6,7 @@ async function sendEmail(req, res) {
   try {
     // console.log("REQ.BODY", req.body);
     await sendgrid.send({
-      to: "mannuarora7000@gmail.com", // Your email where you'll receive emails
+      to: "manuarorawork@gmail.com", // Your email where you'll receive emails
       from: "manuarorawork@gmail.com", // your website email address here
       subject: `[Lead from website] : ${req.body.subject}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
